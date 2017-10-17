b3870a8010886d1acac5397f66b98174d591ea49
David Estes <david@Davids-MBP.domain>
2017-10-17 06:19:12 -0600

    Add initial website structure

---
+++ app/views.py
@@ -0,0 +1,22 @@
+from app import app
+from flask import render_template, g, jsonify, json
+
+from models import PitchforkReviews, MetacriticReviews
+from .forms import PitchforkSearch
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html',
+                           title='Home')
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
